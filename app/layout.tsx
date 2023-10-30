793df92ccfb24ddd81e4a3a66360276b9c4fff85
Rati <rziraqashvili@gmail.com>
2023-10-30 13:56:38 +0400

    Navbar UI and working on modals and button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,33 @@
-import type { Metadata } from 'next'
-import { Nunito } from "next/font/google"
-import './globals.css'
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/modal";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
 
 const font = Nunito({
-  subsets: ["latin"]
-})
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb',
-}
+  title: "Airbnb",
+  description: "Airbnb",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ClientOnly>
+          <Modal isOpen />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
