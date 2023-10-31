534c8a2dcb19a7d3cb47f101105ab233b173a5e4
Rati <rziraqashvili@gmail.com>
2023-10-31 15:44:46 +0400

    Added Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
+import ToasterProvider from "./providers/ToasterProvider";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
