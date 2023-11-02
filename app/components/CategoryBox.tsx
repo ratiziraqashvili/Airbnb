cecc39340def6188c2dfbbbd48650800810b1f93
Rati <rziraqashvili@gmail.com>
2023-11-02 17:21:34 +0400

    User can go between each categories

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -21,10 +21,30 @@ const CategoryBox = ({ icon: Icon, label, selected }: CategoryBoxProps) => {
     if (params) {
       currentQuery = qs.parse(params.toString());
     }
-  }, []);
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
 
   return (
     <div
+      onClick={handleClick}
       className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
         selected ? "border-b-neutral-800" : "border-transparent"
       } ${selected ? "text-neutral-800" : "text-neutral-500"}`}
