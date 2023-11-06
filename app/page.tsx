f58d24b497f7e53049c6704c416a453808dcd9ef
Rati <rziraqashvili@gmail.com>
2023-11-06 14:02:55 +0400

    Fixed type error

--- app/page.tsx
+++ app/page.tsx
@@ -6,26 +6,30 @@ import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
-  const listings = await getListings()
-  const currentUser = await getCurrentUser()
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
-  if(listings.length === 0) {
+  if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
       </ClientOnly>
-    )
+    );
   }
 
   return (
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grids-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-            {listings.map(listing => {
-              return (
-                <ListingCard currentUser={currentUser} key={listing.id} data={listing} />
-              )
-            })}
+          {listings.map((listing) => {
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
         </div>
       </Container>
     </ClientOnly>
