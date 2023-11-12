f34f2136d0761cabb58ca47f686a0ed7c6ab830f
Rati <rziraqashvili@gmail.com>
2023-11-12 14:28:59 +0400

    Changed next config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ const nextConfig = {
             "lh3.googleusercontent.com",
             "res.cloudinary.com"
         ]
-    }
+    },
+    experimental: {
+        appDir: true,
+      },
 }
 
 module.exports = nextConfig
